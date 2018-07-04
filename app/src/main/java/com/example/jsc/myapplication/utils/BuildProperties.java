8342be5648fe1212caeaa1c9196b5fd5f5a7af20
jiaoshichun <731097245@qq.com>
2018-07-04 18:52:31 +0800

    增加修改状态栏颜色和状态栏文字颜色的工具类

---
+++ app/src/main/java/com/example/jsc/myapplication/utils/BuildProperties.java
@@ -0,0 +1,4 @@
+package com.example.jsc.myapplication.utils;
+
+public class BuildProperties {
+}
---
+++ app/src/main/java/com/example/jsc/myapplication/utils/MobileRomUtils.java
@@ -0,0 +1,94 @@
+package com.example.jsc.myapplication.utils;
+
+import android.os.Build;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+
+/**
+ * 终端型号判断的 工具类
+ * 可以判断 小米 魅族 华为
+ */
+public class MobileTypeUtils {
+    private static final String KEY_MIUI_VERSION_CODE = "ro.miui.ui.version.code";
+    private static final String KEY_MIUI_VERSION_NAME = "ro.miui.ui.version.name";
+    private static final String KEY_MIUI_INTERNAL_STORAGE = "ro.miui.internal.storage";
+
+    public static boolean isMIUI() {
+        try {
+            //BuildProperties 是一个工具类，下面会给出代码
+            final BuildProperties prop = BuildProperties.newInstance();
+            return prop.getProperty(KEY_MIUI_VERSION_CODE, null) != null
+                    || prop.getProperty(KEY_MIUI_VERSION_NAME, null) != null
+                    || prop.getProperty(KEY_MIUI_INTERNAL_STORAGE, null) != null;
+        } catch (final IOException e) {
+            return false;
+        }
+    }
+
+    private static final String KEY_EMUI_VERSION_CODE = "ro.build.hw_emui_api_level";
+
+    public static boolean isEMUI() {
+        try {
+            //BuildProperties 是一个工具类，下面会给出代码
+            final BuildProperties prop = BuildProperties.newInstance();
+            return prop.getProperty(KEY_EMUI_VERSION_CODE, null) != null;
+        } catch (final IOException e) {
+            return false;
+        }
+    }
+
+    public static boolean isFlyme() {
+        try {
+            // Invoke Build.hasSmartBar()
+            final Method method = Build.class.getMethod("hasSmartBar");
+            return method != null;
+        } catch (final Exception e) {
+            return false;
+        }
+    }
+
+    /**
+     * 获取miUI版本号
+     * @return
+     */
+    public static int getMiUiVersion() {
+        try {
+            if (isMIUI()) {
+                String systemProperty = getSystemProperty(KEY_MIUI_VERSION_NAME);
+                if (systemProperty != null && systemProperty.length() > 1) {
+                    systemProperty = systemProperty.substring(1);
+                    return Integer.valueOf(systemProperty);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public static String getSystemProperty(String propName) {
+        String line;
+        BufferedReader input = null;
+        try {
+            Process p = Runtime.getRuntime().exec("getprop " + propName);
+            input = new BufferedReader(new InputStreamReader(p.getInputStream()), 1024);
+            line = input.readLine();
+            input.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return line;
+    }
+}
